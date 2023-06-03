ad1b481bbbc8731a1b052ebfdb69168bd910778a
Тарас <Acroyd1984@gmail.com>
2023-06-03 23:06:17 +0300

    Поправил но не работает

--- src/Main.java
+++ src/Main.java
@@ -6,9 +6,11 @@ public class Main {
     public static void main(String[] args) {
         BusinessLogic businessLogic = new BusinessLogic();
         try {
-            businessLogic.account("19120", "1d", "11");
+            businessLogic.account("19120", "1d л", "11");
         } catch (WrongLoginException e) {
+            System.out.println("Неверный логин");
         } catch (WrongPasswordException e) {
+            System.out.println("Неверный пароль");
         }
     }
 }
